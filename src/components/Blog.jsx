78673c0ab7ff0d9f0017f4fbe0968fbeab78088d
A. Urquiola <patchamama.go@gmail.com>
2023-09-16 11:35:09 +0200

    solution 5.7 Blog list frontend, step7. Add button to show blogs details

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}{' '}
+      <span style={hideWhenVisible}>
+        <button onClick={() => setVisible(true)}>view</button>
+      </span>
+      <span style={showWhenVisible}>
+        <button onClick={() => setVisible(false)}>hide</button>
+        <br />
+        {blog.url}
+        <br />
+        likes {blog.likes}{' '}
+        <button onClick={() => console.log('like')}>like</button>
+        <br />
+        {blog.user?.name}
+      </span>
+    </div>
+  )
+}
 
 export default Blog
